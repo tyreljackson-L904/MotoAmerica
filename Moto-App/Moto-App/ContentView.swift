06cc13e19b35a6863916f39cc95c2dddfdff00f0
Tyrel Jackson <tyreljackson904@gmail.com>
2022-06-10 22:28:23 -0600

    First Commit

---
+++ Moto-App/Moto-App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Moto-App
+//
+//  Created by Tyrel Jackson on 6/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Moto-App/Moto-App/Moto_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Moto_AppApp.swift
+//  Moto-App
+//
+//  Created by Tyrel Jackson on 6/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Moto_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
