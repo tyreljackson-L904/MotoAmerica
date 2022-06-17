e3b36b953d5e1ea1be7f8f15da6d42050622d4f1
tyreljackson-L904 <tyreljackson904@gmail.com>
2022-06-17 09:01:41 -0600

    rider detail

--- Moto-App/Moto-App/Views/RiderDetailView.swift
+++ Moto-App/Moto-App/Views/RiderDetailView.swift
@@ -25,6 +25,7 @@ struct RiderDetailView: View {
             }
             .navigationBarHidden(true)
         }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
@@ -94,60 +95,14 @@ struct RiderBioTab: View {
         ScrollView(.vertical, showsIndicators: false) {
             HStack {
                 VStack(alignment: .leading, spacing: 10){
-                    HStack {
-                        Text("Name: ")
-                            .font(.headline)
-                            .fontWeight(.semibold)
-                        + Text("\(rider.name)")
-                            .underline(color: Color.ui.grayBlue)
-                    }
-                    HStack {
-                        Text("Date of Birth: ")
-                            .font(.headline)
-                            .fontWeight(.semibold)
-                        + Text("\(rider.dob)")
-                    }
-                    HStack {
-                        Text("Hometown: ")
-                            .font(.headline)
-                            .fontWeight(.semibold)
-                        + Text("\(rider.hometown)")
-                    }
-                    HStack {
-                        Text("Height: ")
-                            .font(.headline)
-                            .fontWeight(.semibold)
-                        + Text("\(rider.height)")
-                    }
-                    HStack {
-                        Text("Weight: ")
-                            .font(.headline)
-                            .fontWeight(.semibold)
-                        + Text("\(rider.weight)")
-                    }
-                    HStack {
-                        Text("Team: ")
-                            .font(.headline)
-                            .fontWeight(.semibold)
-                        + Text("\(rider.team)")
-                    }
-                    HStack {
-                        Text("Bike: ")
-                            .font(.headline)
-                            .fontWeight(.semibold)
-                        + Text("\(rider.bike)")
-                    }
-                    HStack {
-                        Text("Sponsors: ")
-                            .font(.headline)
-                            .fontWeight(.semibold)
-                    }
-                    HStack {
-                        Text("Biography: ")
-                            .font(.headline)
-                            .fontWeight(.semibold)
-                        + Text("Californian Jake Gagne simply dominated the 2021 MotoAmerica Superbike Series to earn his first Superbike crown and his third AMA Championship, the Fresh N’ Lean Attack Performance Yamaha Racing team leader crushing the record books en route to the title. Gagne won a record 17 out of 20 races on the team’s Yamaha YZF-R1, including a record-setting 16 in a row. Gagne returns to the team in 2022 and will begin the campaign as the heavy favorite to win a second Superbike title.                                                                                                                                                     After suffering a mechanical failure in the opening race of the season at Michelin Raceway Road Atlanta, Gagne went on a tear, winning the next 17 races in a row. He wasn’t stopped until the first of three races in the series finale at Barber Motorsports Park when Mother Nature wreaked havoc with a torrential downpour. In race two at Barber, Gagne finished third and, fittingly, he won the final race of the season on Sunday in Alabama.                                                                                                                      The Superbike Championship is the third AMA title for Gagne as he won the AMA Daytona SportBike Championship in 2015 and was crowned champion of the inaugural MotoAmerica Superstock 1000 Championship in 2015. Gagne won 11 races during his 2015 championship-winning campaign.                                                                                                                                  In 2018, the former youth motocrosser competed overseas in the World Superbike Championship on a Ten Kate Honda but returned stateside to the MotoAmerica Superbike Championship in 2019 and rode a Scheibe Racing BMW to eighth in the title chase.                                                                                                                                            Gagne, who was born and raised in San Diego, California, now resides in Durango, Colorado.                                                                                      Little Known Fact: Jake Gagne qualified for and raced in the 2015 Lucas Oil AMA Pro Motocross Championship round at the Utah Motorsports Campus and very nearly finished in the top 20.")
-                    }
+                    RiderDataRow(rider: rider, label: "Name: ", data: rider.name)
+                    RiderDataRow(rider: rider, label: "Date of Birth: ", data: rider.dob)
+                    RiderDataRow(rider: rider, label: "Hometown: ", data: rider.hometown)
+                    RiderDataRow(rider: rider, label: "Height: ", data:     rider.height)
+                    RiderDataRow(rider: rider, label: "Weight: ", data: String(rider.weight))
+                    RiderDataRow(rider: rider, label: "Team: ", data: rider.team)
+                    RiderDataRow(rider: rider, label: "Bike: ", data: rider.bike)
+                    RiderDataRow(rider: rider, label: "Sponsors: ", data: rider.sponsors.joined(separator: ", "))
                 }
                 .padding()
                 
@@ -214,6 +169,24 @@ struct TimingScoringTab: View {
 // Preview
 struct RiderDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        RiderDetailView(rider: RiderList.riders[8])
+        RiderDetailView(rider: RiderList.riders[0])
+            .previewInterfaceOrientation(.portrait)
+    }
+}
+
+// Rider data row for Bio
+struct RiderDataRow: View {
+    
+    var rider: Rider
+    var label: String
+    var data: String
+    
+    var body: some View {
+        HStack {
+            Text(label)
+                .font(.headline)
+                .fontWeight(.semibold)
+            + Text(data)
+        }
     }
 }
